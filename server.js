54f8f714091c51ac1cf879434170124e94c68e6d
Luiz Fumes <luiz.fumes@dafiti.com.br>
2017-03-13 11:28:22 -0300

    Removed configs

--- server.js
+++ server.js
@@ -2,14 +2,6 @@
 
 const Hapi = require('hapi');
 
-const env = process.env.NODE_ENV;
-if (env == undefined) {
-  console.log("NODE_ENV not defined");
-  process.exit();
-}
-
-global.Config = require('./config/config.' + env + '.json');
-
 const server = new Hapi.Server();
 server.connection({ port: 3030, routes: { cors: true }});
 
